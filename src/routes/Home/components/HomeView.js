b2cc85c4422fec307dedeec978476b668b55fe9d
Alicja Kujawa <alicja.kujawa@monterail.com>
2017-04-09 06:06:03 +0200

    Add dots physics

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import './HomeView.scss'
 import song from '../assets/shinedown-heroes.mp3'
 import chroma from 'chroma-js'
-import { Circle } from '../utils/Shape'
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)()
 
@@ -14,7 +13,44 @@ function fetchSong (src, cb) {
     .catch(e => console.error(e))
 }
 
-const circles = []
+const sumRange = (buf, min, max) => {
+  let sum = 0
+  for (let i = min; i < max; i++) {
+    sum += buf[i]
+  }
+  return sum
+}
+
+const linearPeak = (peakVal, from, to) => {
+  const i = 1 / (peakVal + 1)
+  return to * i + from * (1 - i)
+}
+
+const makePeakDetector = (min, max) => {
+  let lastPeak = 0
+  let threshold = 0
+  const decay = (max - min) / 2
+  return {
+    feed (buf) {
+      const data = sumRange(buf, min, max)
+      threshold = Math.max(0, threshold - decay)
+      if (data > threshold) {
+        lastPeak = 0
+        threshold = data + decay * 5
+      } else {
+        lastPeak++
+      }
+      return lastPeak
+    },
+    get () {
+      return {
+        v: lastPeak,
+        min,
+        max
+      }
+    }
+  }
+}
 
 class HomeView extends Component {
 
@@ -27,85 +63,112 @@ class HomeView extends Component {
     sourceNode.connect(this.analyser)
     this.analyser.connect(audioContext.destination)
     sourceNode.start()
+    let bands = []
+    // for (let i = 0; i < this.analyser.frequencyBinCount / 64; i++) {
+    //   bands.push([i * 64, (i + 1) * 64])
+    // }
+    const bins = this.analyser.frequencyBinCount
+    bands.push([0, Math.floor(bins - 1)])
+    bands.push([Math.floor(bins / 4), Math.floor(bins * 2 / 3)])
+    bands.push([0, Math.floor(bins / 5)])
+    bands.push([Math.floor(bins * 3 / 4), Math.floor(bins - 1)])
+
+    this.peaks = bands.map(([a, b]) => makePeakDetector(a, b))
+    this.peakAll = this.peaks[0]
+    this.peakLows = this.peaks[1]
+    this.peakMids = this.peaks[2]
+    this.peakHighs = this.peaks[3]
+
+    const w = this.canvas.width
+    const h = this.canvas.height
+
+    this.dots = Array.from({ length: 20 }, () => ({
+      x: Math.random() * w,
+      y: Math.random() * h,
+      vx: Math.random() * 10 - 5,
+      vy: Math.random() * 10 - 5
+    }))
+
+    this.gravity = {
+      x: w / 2,
+      y: h / 2,
+      g: 0.1
+    }
   }
 
   componentDidMount () {
     fetchSong(this.refs.audioEl.src, (audioBuffer) => {
       this.initAnalyzer(audioBuffer)
-      this.drawCircles()
       this.startAnim()
     })
   }
 
   startAnim () {
-    requestAnimationFrame((dt) => {
-      this.tick(dt)
+    let prevT = performance.now()
+    requestAnimationFrame((T) => {
+      const dt = T - prevT
+      prevT = T
+      this.tick(dt / 1000)
       this.startAnim()
     })
   }
 
-  drawCircles () {
+  tick (dt) {
     const ctx = this.canvas.getContext('2d')
-    const colorScale = chroma.scale(['#000000', '#ff0000', '#ffff00', '#ffffff']).out('hex')
+    // const colorScale = chroma.scale(['#000000', '#ff0000', '#ffff00', '#ffffff']).out('hex')
     const domain = this.freqDomain
     this.analyser.getByteFrequencyData(domain)
+    this.peaks.forEach(peak => peak.feed(domain))
 
-    for (let i = 0; i < domain.length; i++) {
-      // const index = Math.ceil(domain.length / 20 * i)
-      const value = Math.pow((domain[i] / 255), 2) * 255
-      const randomX = Math.round(Math.random() * 800)
-      const randomY = Math.round(Math.random() * 800)
-      const speed = value * 0.2
-      const radius = value
-      const circle = new Circle(radius, speed, randomX, randomY, colorScale(value / 256))
-      circles.push(circle)
-      circle.update(ctx)
+    let total = domain.length
+    const w = this.canvas.width
+    const h = this.canvas.height
+    const size = linearPeak(this.peakMids.get().v, 5, 50)
+    // this.gravity.g = linearPeak(this.peakHighs.get().v, 1, -2)
+    // ctx.fillStyle = 'white'
+    // ctx.globalAlpha = 0.2
+    // ctx.fillRect(0, 0, w, h)
+    // ctx.fillStyle = 'black'
+    // ctx.globalAlpha = 1
+
+    ctx.clearRect(0, 0, w, h)
+
+    this.gravity.g = 20
+
+    this.dots.forEach(dot => {
+      const dx = (this.gravity.x - dot.x) / 150
+      const dy = (this.gravity.y - dot.y) / 150
+      const ax = this.gravity.g * dt / dx
+      const ay = this.gravity.g * dt / dy
+      dot.vx += ax
+      dot.vy += ay
+      // dot.vx *= 0.95
+      // dot.vy *= 0.95
+
+      dot.x += dot.vx
+      dot.y += dot.vy
+
+      if (dot.x < 0) { dot.x = 0; dot.vx = 0 }
+      if (dot.x > w) { dot.x = w; dot.vx = 0 }
+      if (dot.y < 0) { dot.y = 0; dot.vy = 0 }
+      if (dot.y > h) { dot.y = h; dot.vy = 0 }
+
+      ctx.fillRect(dot.x - size / 2, dot.y - size / 2, size, size)
+    })
+
+    const fd = f => f / (total + 1) * w
+    const drawPeak = (peak) => {
+      const m = peak.get()
+      let ph = (10 / (m.v + 10)) * 200
+      let p = 360 * (m.min + m.max) / 2048
+      ctx.strokeStyle = `hsla(${p},100%,50%,0.5)`
+      ctx.beginPath()
+      ctx.lineWidth = ph
+      ctx.moveTo(fd(m.min), h / 2)
+      ctx.lineTo(fd(m.max), h / 2)
+      ctx.stroke()
     }
-  }
-
-  getMagAverage (data, min, max) {
-    const slicedData = data.slice(min, max)
-    return slicedData.reduce((acc, val) => acc + val, 0) / (max - min + 1)
-  }
-
-  peakDetector (min, max) {
-    const decay = (max - min) / 2
-    let threshold = 0
-    let lastPeak = 0
-
-    return {
-      process (data) {
-        const mag = this.getMagAverage(data, min, max)
-        threshold = Math.max(0, threshold - decay)
-        if (mag > threshold) {
-          lastPeak = 0
-          threshold = mag + decay * 5
-        } else {
-          lastPeak++
-        }
-        return lastPeak
-      },
-      get () {
-        return lastPeak
-      }
-    }
-  }
-
-  tick () {
-    const ctx = this.canvas.getContext('2d')
-    const colorScale = chroma.scale(['#000000', '#ff0000', '#ffff00', '#ffffff']).out('hex')
-    const domain = this.freqDomain
-    this.analyser.getByteFrequencyData(domain)
-    ctx.clearRect(0, 0, 800, 1024)
-
-    for (let i = 0; i < domain.length; i++) {
-      // const index = Math.ceil(domain.length / 20 * i)
-      const value = Math.pow((domain[i] / 255), 2) * 255
-      const speed = value * 0.2
-      const radius = value
-      circles[i].set(radius, speed, colorScale(value / 256))
-      circles[i].update(ctx)
-    }
+    this.peaks.forEach(drawPeak)
   }
 
   render () {
@@ -113,7 +176,7 @@ class HomeView extends Component {
       <div>
         <h4>Music player!</h4>
         <audio src={song} ref='audioEl' />
-        <canvas width='800' height='1024' ref={c => { this.canvas = c }} />
+        <canvas height='768' width='1024' ref={c => { this.canvas = c }} />
       </div>
     )
   }
